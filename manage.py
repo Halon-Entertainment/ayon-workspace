b41036ed424ef31c235659be0a43d1c8f04492ad
Chad Provencher <thechad802@gmail.com>
2024-08-13 12:06:39 -0400

    added cli commands

--- manage.py
+++ manage.py
@@ -1,20 +1,12 @@
 import pathlib
 import subprocess
 import toml
+import click
 
 
 ROOT_PATH = pathlib.Path(__file__).parent.resolve()
 project_data = toml.load(ROOT_PATH / "pyproject.toml")
 
-
-
-def get_repositories():
-    for category, data in project_data["tool"]["ayon-workspace"]["git"].items():
-        if category == "docker":
-            category = "repos/ayon-docker"
-        for name, url in data.items():
-            path = ROOT_PATH / category / name
-            get_repository(name, path, url)
 
 def get_repository(repository_name, path, repo_url):
     if path.exists():
@@ -26,5 +18,19 @@ def get_repository(repository_name, path, repo_url):
     subprocess.call(git_command, shell=True)
 
 
+@click.group()
+def cli():
+    pass
+
+@cli.command(help="Pulls all configured repositiories, see pyproject.toml.")
+def get_repositories():
+    for category, data in project_data["tool"]["ayon-workspace"]["git"].items():
+        if category == "docker":
+            category = "repos/ayon-docker"
+        for name, url in data.items():
+            path = ROOT_PATH / category / name
+            get_repository(name, path, url)
+
+
 if __name__ == "__main__":
-    get_repositories()
+    cli()
