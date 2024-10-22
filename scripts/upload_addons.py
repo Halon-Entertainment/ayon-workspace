025859070f56fcd21b492bbdba318610a1f7cb0f
Chad Provencher <thechad802@gmail.com>
2024-10-22 09:25:07 -0400

    added create package only

--- scripts/upload_addons.py
+++ scripts/upload_addons.py
@@ -42,11 +42,17 @@ def create_package(addon_name: str):
     addon_folder = ADDONS_FOLDER / addon_name
     cmd = f'poetry install && poetry env use python && python ./create_package.py'
     subprocess.run(cmd, cwd=addon_folder.as_posix(), shell=True)
+    print(f"Package Created: {addon_folder.as_posix()}")
 
-@click.command(name='upload-addons', help='Creates packages and uploads them to the server configured in the .env file.')
-@click.argument('addons', default=[])
+def create_packages(addons):
+    for addon in addons:
+        create_package(addon)
+
+@click.command(name='addons', help='Creates packages and uploads them to the server configured in the .env file.')
+@click.argument('addons', nargs=-1)
 @click.option('-a', '--all-addons', is_flag=True, default=False)
-def upload_addons_cli(addons, all_addons=False):
+@click.option('-c', '--create-package-only', is_flag=True, default=False)
+def upload_addons_cli(addons, all_addons=False, create_package_only=False):
     if not all_addons:
         vaild_addons = set([x.name for x in ADDONS_FOLDER.iterdir() if x.is_dir()])
         addons_set = set(addons)
@@ -62,7 +68,10 @@ def upload_addons_cli(addons, all_addons=False):
     else:
         addons = [x.name for x in ADDONS_FOLDER.iterdir() if x.is_dir()]
 
-    upload_addons(addons)
+    if not create_package_only:
+        upload_addons(addons)
+    else:
+        create_packages(addons)
 
 if __name__ == '__main__':
     upload_addons_cli()
